9413b0143a5335cd7a2dc89b2b386236732f446f
shakilhasan <shakilhasan105268@gmail.com>
2022-01-12 19:01:49 +0600

    add heroku serverEndpoint

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,8 @@
 import socketIOClient from "socket.io-client";
 
 // const serverEndpoint = "<your-nodejs-server-url>";
-const serverEndpoint = "http://localhost:8080";
+// const serverEndpoint = "http://localhost:8080";
+const serverEndpoint = "https://mychat-api.herokuapp.com";
 
 export const socket = socketIOClient(serverEndpoint, {
     transports: ['websocket']
